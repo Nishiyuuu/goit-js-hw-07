62e865bce27a0bb45034b93d9566788f4014fe2c
Nishiyuu <bickovskijvlad53@gmail.com>
2025-07-16 00:55:49 +0200

    last task is done

--- src/js/task-5.js
+++ src/js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  color.textContent = newColor;
+})
