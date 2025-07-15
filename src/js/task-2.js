3acdbf9263b2a390ac05fe70ea967599a25fc192
Nishiyuu <bickovskijvlad53@gmail.com>
2025-07-16 00:06:19 +0200

    second task comleated

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -24,3 +24,13 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = images
+  .map(({ url, alt }) => `<li class="gallery-item"><img src="${url}" alt="${alt}" class="gallery-image" width=360 height=300/></li>`)
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
